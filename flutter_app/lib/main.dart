6ec6b48a9bd6e0ce518ea439f5401f376ec11679
jamelchadha <chadhajamel47@gmail.com>
2023-07-17 17:50:28 +0200

    login && register

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,45 +1,22 @@
 import 'package:flutter/material.dart';
-import 'Home/HomePage.dart';
+
+import 'Feature/Login Screen/Login_Screen.dart';
 
 void main() {
-  runApp( const MyApp(  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ordering delivry pharmaceutique',
-      home:  MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      backgroundColor: Colors.white,
-        body: SafeArea(
-          child: HomePage() ,
-          ),
+      title: 'Doctor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
     );
   }
 }
