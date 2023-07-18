5e44ada808b0cb475776f9b517475eeca55c3c64
med363 <blibechmedamine@gmail.com>
2023-07-18 21:12:54 +0100

    add images

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, // Set to false to remove the debug banner
       //  theme: ThemeData(
           // drawerTheme: const DrawerThemeData(scrimColor: Colors.white)),
 
