bf8e2df39160fabfbf201d6d0d92694eba227347
med363 <blibechmedamine@gmail.com>
2023-07-19 21:39:58 +0100

    add card

--- flutter_app/lib/Home/HomePage.dart
+++ flutter_app/lib/Home/HomePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../Core/Colors/Hex_Color.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,69 +13,74 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   //final padding = EdgeInsets.symmetric(horizontal: 20);
   // final GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   final List<String> images = [
     'med.png',
     'medicament.png',
     'pharmacie.png',
   ];
-  final List<String> boxColors = [
-    'Red',
-    'Blue',
-    'Green',
-    'Yellow',
-    'Orange',
-    'Purple',
-  ];
+
   @override
   Widget build(BuildContext context) {
-        return 
-         Scaffold(
+    return Scaffold(
+        key: _scaffoldKey,
         extendBodyBehindAppBar: true, // Important: extend body behind AppBar
 
         appBar: AppBar(
-          
-
           backgroundColor: Colors.transparent,
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.black),
           // Set the color of the drawer icon
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text(
-                "Log in",
-                style: TextStyle(color: Colors.black),
+          leading: IconButton(
+            icon: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: SvgPicture.asset(
+                'assets/images/menu.svg', // Replace with the path to your SVG file
+                width: 24,
+                height: 24,
               ),
             ),
-            const Icon(Icons.person)
+            onPressed: () {
+              _scaffoldKey.currentState?.openDrawer();
+            },
+          ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: IconButton(
+                icon: Icon(Icons.login),
+                onPressed: () {
+                Navigator.pushNamed(context, '/login');
+                },
+              ),
+            ),
           ],
         ),
         //inside the body
         body: Stack(children: [
           Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            stops: const [0.1, 0.4, 0.7, 0.9],
-            colors: [
-              HexColor("#4b4293").withOpacity(0.8),
-              HexColor("#4b4293"),
-              HexColor("#08418e"),
-              HexColor("#08418e")
-            ],
-          ),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                HexColor("#fff").withOpacity(0.2), BlendMode.dstATop),
-            image: AssetImage('assets/images/1.jpg'),
-          ),
-        ),
+            // decoration: BoxDecoration(
+            //   gradient: LinearGradient(
+            //     begin: Alignment.topLeft,
+            //     end: Alignment.bottomRight,
+            //     stops: const [0.1, 0.4, 0.7, 0.9],
+            //     colors: [
+            //       HexColor("#4b4293").withOpacity(0.6),
+            //       HexColor("#4b4293").withOpacity(0.6),
+            //       HexColor("#08418e"),
+            //       HexColor("#08418e")
+            //     ],
+            //   ),
+            //   image: DecorationImage(
+            //     fit: BoxFit.cover,
+            //     colorFilter: ColorFilter.mode(
+            //         HexColor("#fff").withOpacity(0.3), BlendMode.dstATop),
+            //     image: AssetImage('assets/images/1.jpg'),
+            //   ),
+            // ),
 
-            // color: Colors.white,
+            color: Colors.white38,
             // decoration: BoxDecoration(
             //   image: DecorationImage(
             //       imacloge: AssetImage("../../icons/background.jpg"),
@@ -95,67 +100,114 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          Container(
-              padding: const EdgeInsets.only(top: 160, left: 10, right: 10),
-              height: 200, // Adjust the height as needed
-              child: ListView.builder(
-                  scrollDirection:
-                      Axis.horizontal, // Set the scroll direction to horizontal
-                  itemCount: images.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                        width: 150, // Adjust the width as needed
-                        margin: EdgeInsets.symmetric(horizontal: 8),
-                        color:
-                            _getColorByIndex(index), // Get color based on index
-                        child: Center(
-                            child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.asset(
-                            'assets/images/${images[index]}', // Replace with your image path
-                            fit: BoxFit.cover,
-                          ),
-                        )));
-                  }))
+Container(
+  padding: const EdgeInsets.only(top: 160, left: 10, right: 10),
+  height: 500,
+  child: ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: images.length,
+    itemBuilder: (context, index) {
+      return Container(
+        width: 200,
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        child: Card(
+          elevation: 4, // Adjust the elevation value for the shadow effect
+          color: Color(0xFFABCDEF), // Replace with your desired "crystal" color
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/images/${images[index]}',
+                  fit: BoxFit.contain, // Adjust the fit property for resizing the image
+                  width: 120, // Adjust the width value as needed
+                  height: 120, // Adjust the height value as needed
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Name', // Replace with the actual name
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  ),
+)
+
+
         ]),
         drawer: Drawer(
             width: 275,
             elevation: 30,
-            backgroundColor: Color(0xF3393838),
+            backgroundColor: Colors.white,
             shape: const RoundedRectangleBorder(
                 borderRadius:
                     BorderRadius.horizontal(right: Radius.circular(30))),
             child: Container(
-                decoration: const BoxDecoration(
-                    borderRadius:
-                        BorderRadius.horizontal(right: Radius.circular(40)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.white, spreadRadius: 10, blurRadius: 10)
-                    ]),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: const [0.1, 0.4, 0.7, 0.9],
+                    colors: [
+                      HexColor("#4b4293").withOpacity(0.5),
+                      HexColor("#4b4293").withOpacity(0.8),
+                      HexColor("#08418e"),
+                      HexColor("#08418e")
+                    ],
+                  ),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        HexColor("#fff").withOpacity(0.3), BlendMode.dstATop),
+                    image: AssetImage('assets/images/1.jpg'),
+                  ),
+                ),
                 child: Padding(
                     padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(children: [
-                            Row(
-                              children: const [
-                                Icon(
-                                  Icons.arrow_back_ios,
-                                  color: Colors.black,
-                                  size: 20,
-                                ),
-                                SizedBox(
-                                  width: 56,
-                                ),
-                                Text(
-                                  'Back ',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 16),
-                                ),
-                              ],
-                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).pop(); // Close the drawer
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Icon(
+                                    Icons.arrow_back_ios,
+                                    color: Colors.black,
+                                    size: 20,
+                                  ),
+                                  SizedBox(
+                                    width: 0,
+                                  ),
+                                  Text(
+                                    'Back ',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            )
                           ]),
                         ])))));
   }
